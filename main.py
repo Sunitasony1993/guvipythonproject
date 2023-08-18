b207577c338b3373c585079dd446b0c28b819725
Saswat kumar Mohanty <devanshpc@Saswats-Mac-mini.local>
2023-08-18 11:38:28 +0530

    Initial commit - First project comiited to Git

---
+++ main.py
@@ -0,0 +1,34 @@
+import math
+
+print("hello debu")
+
+# print(type(10))
+# print(type(10.))
+# print(3j)
+
+# rint(type(3j))
+# print(type(2e4))
+
+# print(10 + 6)
+print(10 - 6)
+print(10 * 6)
+print(10 / 6)
+print(10 % 6)
+print(10 // 6)
+# BODMAS RULE
+print(10 + 5 - 3 / 2 * 4)
+
+print(min(12, 43, 64, -23, 12, -421, -1, -21, 124, -315))
+print(round(11.5))
+print(round(8.5))
+print(round(34.3553, 1))
+print(round(45.48656, 3))
+print(sum((10, 20, 48, -24, 134, 431, 6, -341)))
+print(3 - - 3)
+print(3 + - 3)
+
+# math.sqrt()
+print(math.sqrt(16))
+print(math.cbrt(27))
+
+# math.ceil()
