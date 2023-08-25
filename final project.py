a7513d364d58186b5e2fe31f60dcfa35f01bf5cb
Sunita <sunitasony0@gmail.com>
2023-08-25 13:18:31 +0530

    Final Project with all bug fixes V2.0 insert window

--- final project.py
+++ final project.py
@@ -1,9 +1,9 @@
 from tkinter import *
 import math
-root = Tk()
-root.title("calculator")
-root.geometry("680x356+100+100")
-root.config(bg="black")
+window = Tk()
+window.title("calculator")
+window.geometry("680x356+100+100")
+window.config(bg="black")
 font = ("Arial black", 15)
 # FUNCTIONAL PART:-
 
@@ -59,82 +59,82 @@ def clear():
 
 
 # GUI part
-entry_result = Entry(root, font=font, bg="#21618C", bd=12,
+entry_result = Entry(window, font=font, bg="#21618C", bd=12,
                      fg="#FBFCFC", relief=SUNKEN, width=58)
 entry_result.grid(row=0, column=0, columnspan=5)
-clear_result = Button(root, text="C", font=font, bg="white", relief=GROOVE,
+clear_result = Button(window, text="C", font=font, bg="white", relief=SUNKEN,
                       fg="blue", height=2, width=1, command=lambda: clear())
 clear_result.grid(row=1, column=0, sticky="news")
-square = Button(root, text="x2", font=font, bg="white", relief=SUNKEN,
+square = Button(window, text="x2", font=font, bg="white", relief=SUNKEN,
                 fg="blue", height=2, width=1, command=lambda: button_press("x2"))
 square.grid(row=1, column=1, sticky="news")
-cube = Button(root, text="x3", font=font, bg="white", relief=SUNKEN,
+cube = Button(window, text="x3", font=font, bg="white", relief=SUNKEN,
               fg="blue", height=2, width=1, command=lambda: button_press("x3"))
 cube.grid(row=1, column=2, sticky="news")
-square_root = Button(root, text="√", font=font, bg="white", relief=SUNKEN,
+square_root = Button(window, text="√", font=font, bg="white", relief=SUNKEN,
                      fg="blue", height=2, width=1, command=lambda: button_press("√"))
 square_root.grid(row=1, column=3, sticky="news")
-division = Button(root, text="/", font=font, bg="white", relief=SUNKEN,
+division = Button(window, text="/", font=font, bg="white", relief=SUNKEN,
                   fg="blue", height=2, width=1, command=lambda: button_press("/"))
 division.grid(row=1, column=4, sticky="news")
-multiply = Button(root, text="*", font=font, bg="white", relief=SUNKEN,
+multiply = Button(window, text="*", font=font, bg="white", relief=SUNKEN,
                   fg="blue",  height=2, width=1, command=lambda: button_press("*"))
 multiply.grid(row=2, column=4, sticky="news")
-cube_root = Button(root, text="∛", font=font, bg="white", relief=SUNKEN,
+cube_root = Button(window, text="∛", font=font, bg="white", relief=SUNKEN,
                    fg="blue",  height=2, width=1, command=lambda: button_press("∛"))
 cube_root.grid(row=2, column=3, sticky="news")
-num_9 = Button(root, text="9", font=font, bg="white", relief=SUNKEN,
+num_9 = Button(window, text="9", font=font, bg="white", relief=SUNKEN,
                fg="blue", height=2, width=1, command=lambda: button_press("9"))
 num_9.grid(row=2, column=2, sticky="news")
-num_8 = Button(root, text="8", font=font, bg="white", relief=SUNKEN,
+num_8 = Button(window, text="8", font=font, bg="white", relief=SUNKEN,
                fg="blue", height=2, width=1, command=lambda: button_press("8"))
 num_8.grid(row=2, column=1, sticky="news")
-num_7 = Button(root, text="7", font=font, bg="white", relief=SUNKEN,
+num_7 = Button(window, text="7", font=font, bg="white", relief=SUNKEN,
                fg="blue", height=2, width=1, command=lambda: button_press("7"))
 num_7.grid(row=2, column=0, sticky="news")
-num_4 = Button(root, text="4", font=font, bg="white", relief=SUNKEN,
+num_4 = Button(window, text="4", font=font, bg="white", relief=SUNKEN,
                fg="blue", height=2, width=1, command=lambda: button_press("4"))
 num_4.grid(row=3, column=0, sticky="news")
-num_5 = Button(root, text="5", font=font, bg="white", relief=SUNKEN,
+num_5 = Button(window, text="5", font=font, bg="white", relief=SUNKEN,
                fg="blue", height=2, width=1, command=lambda: button_press("5"))
 num_5.grid(row=3, column=1, sticky="news")
-num_6 = Button(root, text="6", font=font, bg="white", relief=SUNKEN,
+num_6 = Button(window, text="6", font=font, bg="white", relief=SUNKEN,
                fg="blue", height=2, width=1, command=lambda: button_press("6"))
 num_6.grid(row=3, column=2, sticky="news")
-pie_val = Button(root, text="𝞹", font=font, bg="white", relief=SUNKEN,
+pie_val = Button(window, text="𝞹", font=font, bg="white", relief=SUNKEN,
                  fg="blue", height=2, width=1, command=lambda: button_press("𝞹"))
 pie_val.grid(row=3, column=3, sticky="news")
-plus = Button(root, text="+", font=font, bg="white", relief=SUNKEN,
+plus = Button(window, text="+", font=font, bg="white", relief=SUNKEN,
               fg="blue", height=2, width=1, command=lambda: button_press("+"))
 plus.grid(row=3, column=4, sticky="news")
-num_1 = Button(root, text="1", font=font, bg="white", relief=SUNKEN,
+num_1 = Button(window, text="1", font=font, bg="white", relief=SUNKEN,
                fg="blue", height=2, width=1, command=lambda: button_press("1"))
 num_1.grid(row=4, column=0, sticky="news")
-num_2 = Button(root, text="2", font=font, bg="white", relief=SUNKEN,
+num_2 = Button(window, text="2", font=font, bg="white", relief=SUNKEN,
                fg="blue", height=2, width=1, command=lambda: button_press("2"))
 num_2.grid(row=4, column=1, sticky="news")
-num_3 = Button(root, text="3", font=font, bg="white", relief=SUNKEN,
+num_3 = Button(window, text="3", font=font, bg="white", relief=SUNKEN,
                fg="blue", height=2, width=1, command=lambda: button_press("3"))
 num_3.grid(row=4, column=2, sticky="news")
-ln_value = Button(root, text="ln", font=font, bg="white", relief=SUNKEN,
+ln_value = Button(window, text="ln", font=font, bg="white", relief=SUNKEN,
                   fg="blue", height=2, width=1, command=lambda: button_press("ln"))
 ln_value.grid(row=4, column=3, sticky="news")
-minus = Button(root, text="-", font=font, bg="white", relief=SUNKEN,
+minus = Button(window, text="-", font=font, bg="white", relief=SUNKEN,
                fg="blue", height=2, width=1, command=lambda: button_press("-"))
 minus.grid(row=4, column=4, sticky="news")
-zero = Button(root, text="0", font=font, bg="white", relief=SUNKEN,
+zero = Button(window, text="0", font=font, bg="white", relief=SUNKEN,
               fg="blue", height=2, width=1, command=lambda: button_press("0"))
 zero.grid(row=5, column=0, sticky="news")
-point = Button(root, text=".", font=font, bg="white", relief=SUNKEN,
+point = Button(window, text=".", font=font, bg="white", relief=SUNKEN,
                fg="blue", height=2, width=1, command=lambda: button_press("."))
 point.grid(row=5, column=1, sticky="news")
-log_value = Button(root, text="log10", font=("Arial black", 12), bg="white", relief=SUNKEN,
+log_value = Button(window, text="log10", font=("Arial black", 12), bg="white", relief=SUNKEN,
                    fg="blue", height=2, width=1, command=lambda: button_press("log10"))
 log_value.grid(row=5, column=2, sticky="news")
-e_value = Button(root, text="e", font=font, bg="white", relief=SUNKEN,
+e_value = Button(window, text="e", font=font, bg="white", relief=SUNKEN,
                  fg="blue", height=2, width=1, command=lambda: button_press("e"))
 e_value.grid(row=5, column=3, sticky="news")
-equal_to = Button(root, text="=", font=font, bg="white", relief=SUNKEN,
+equal_to = Button(window, text="=", font=font, bg="white", relief=SUNKEN,
                   fg="blue", height=2, width=1, command=lambda: calculate())
 equal_to.grid(row=5, column=4, sticky="news")
-root.mainloop()
+window.mainloop()
